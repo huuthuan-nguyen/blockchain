2602f42b16e2cd5342defb5bb9ac1fb7089591a9
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2017-12-18 23:10:50 +0700

    Update Mining Feature

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
+import requests
+import json
+from main import blockchain
+from block import Block
+import datetime as date
 node = Flask(__name__)
 
 
 # Store the transaction that
 # this node has in a list
 this_nodes_transactions = []
+# Store the url data of every
+# other node in the network
+# so the we can communicate
+# with them
+peer_nodes = []
+# A variable to deciding if we're mining or not
+mining = True
 
 
 @node.route('/txion', methods=["POST"])
@@ -43,6 +55,105 @@ def proof_of_work(last_proof):
     # we can return it as a proof
     # of our work
     return incrementor
+
+
+@node.route('/mine', methods=['GET'])
+def mine():
+    # Get the last proof work
+    last_block = blockchain[len(blockchain)-1]
+    if isinstance(last_block.data, dict) and 'proof-of-work' in last_block.data:
+        last_proof = last_block.data['proof-of-work']
+    else:
+        last_proof = 9
+    # Find the proof of work for
+    # the current block being mined
+    # Note: The program will hang here until a new
+    # proof of work is found
+    proof = proof_of_work(last_proof)
+    # Once we find  a valid proof of work,
+    # we know we can mine a block so
+    # we reward the miner by adding a transaction
+    this_nodes_transactions.append(
+        {"from": "network", "to": miner_address, "amount": 1}
+    )
+    # Now we can gather the data needed
+    # to create the new block
+    new_block_data = {
+        "proof-of-work": proof,
+        "transactions": list(this_nodes_transactions)
+    }
+    new_block_index = last_block.index + 1
+    new_block_timestamp = this_timestamp = date.datetime.now()
+    last_block_hash = last_block.hash
+    # Empty transaction list
+    this_nodes_transactions[:] = []
+    # Now create the
+    # new block!
+    mined_block = Block(
+        new_block_index,
+        new_block_timestamp,
+        new_block_data,
+        last_block_hash
+    )
+    blockchain.append(mined_block)
+    # Let the client know we mined a block
+    return jsonify({
+        "index": new_block_index,
+        "timestamp": str(new_block_timestamp),
+        "data": new_block_data,
+        "hash": last_block_hash
+    })
+
+
+@node.route('/blocks', methods=['GET'])
+def get_blocks():
+    chain_to_send = blockchain
+    blocklist = list()
+    # Convert our blocks into dictionaries
+    # so we can send them as json objects later
+    for i in range(len(chain_to_send)):
+        block = chain_to_send[i]
+        block_index = str(block.index)
+        block_timestamp = str(block.timestamp)
+        block_data = str(block.data)
+        block_hash = str(block.hash)
+        assembled = {
+            "index": block_index,
+            "timestamp": block_timestamp,
+            "data": block_data,
+            "hash": block_hash
+        }
+        blocklist.append(assembled)
+
+    return jsonify(blocklist)
+
+
+def find_new_chains():
+    # Get the blockchains of every
+    # other node
+    other_chains = []
+    for node_url in peer_nodes:
+        # Get their chains using a GET request
+        block = requests.get(node_url + "/blocks").content
+        # Convert the JSON object to Python dictionary
+        block = json.loads(block)
+        # Add it to our list
+        other_chains.append(block)
+    return other_chains
+
+
+def consensus():
+    # Get the block from other nodes
+    other_chains = find_new_chains()
+    # If our chain isn't longest,
+    # then we store the longest chain
+    longest_chain = blockchain
+    for chain in other_chains:
+        if len(longest_chain) < len(chain):
+            longest_chain = chain
+    # If the longest chain wasn't ours,
+    # then we set our chain to the longest
+    blockchain = longest_chain
 
 
 node.run()
